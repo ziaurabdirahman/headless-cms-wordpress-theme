c90f93e1f6831106e2dc369ec367b38945a3624b
Ziaur Abdirahman <adtechzia@gmail.com>
2019-08-02 23:33:02 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Headless CMS</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+<style>
+  @import url("https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700,800&display=swap");
+
+body {
+  font-family: "Montserrat", sans-serif;
+  background-color: #323c46;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  color: #fff;
+}
+</style>
+
+</head>
+<body>
+  <div class="container">
+   <div class="my-5">
+   <h1>This Theme is for Headless Wordpress</h1>
+    <p>Creadted by <a href="https://adtechsoft.com"><b>Ziaur Abdirahman</b></a></p>
+   </div>
+   <hr>
+   <div>
+     <h4 class="mb-3">Some important Endpoints links:</h4>
+     <p>All Endpoints: <span class="text-warning">https://your-site.com/wp-json/</span></p>
+     <p>All Posts: <span class="text-warning">https://your-site.com/wp-json/wp/v2/posts/</span></p>
+     <p>Single Post: <span class="text-warning">https://your-site.com/wp-json/wp/v2/posts/id</span></p>
+     <p>All Pages: <span class="text-warning">https://your-site.com/wp-json/wp/v2/pages/</span></p>
+     <p>Single Page: <span class="text-warning">https://your-site.com/wp-json/wp/v2/pages/id</span></p>
+     <p>All Users: <span class="text-warning">https://your-site.com//wp-json/wp/v2/users/</span></p>
+   </div>
+  </div>
+</body>
+</html>
